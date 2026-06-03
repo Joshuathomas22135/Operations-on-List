37e2b697ba30ef05ddfb4ea551971bed4762c725
Joshua Thomas <134388878+Joshuathomas22135@users.noreply.github.com>
2026-06-03 19:33:28 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Write a Python program to create a Python list of your favourite fruits (minimum 5). Then perform the given operations on it.
+
+list = ["Apple", "Orange", "Banana", "Avacado", "Kiwi"]
+print(list)
+
+list.append("Pineapple")
+print(list)
+
+list.remove("Orange")
+print(list)
+
+list.sort()
+print(list)
+
+print(list[-1])
+print(list[2:4])
+print(list[::-1])
